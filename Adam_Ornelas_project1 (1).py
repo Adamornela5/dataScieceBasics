9187acb6baf8cb5c0c1418a8a406f2eb64fcd40b
Adam Ornelas <adam.ornelas10@gmail.com>
2025-10-30 16:53:27 -0700

    r code

---
+++ Adam_Ornelas_project1 (1).py
@@ -0,0 +1,5 @@
+import pandas as pd
+myDF = pd.read_csv("/anvil/projects/tdm/data/flights/subset/airports.csv")
+myDF.head()
+
+We imported the first five rows of a data set from a csv file in python. We did this by importing pandas and creating the data frame labeled myDF.
